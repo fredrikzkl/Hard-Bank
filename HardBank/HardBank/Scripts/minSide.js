0c7f93ff65a2d0e84b6e147d9f855f2e4ba244c8
Fredrik Kloster <fredrik_kloster@hotmail.com>
2016-10-15 17:35:36 +0200

    datepicker added

--- HardBank/HardBank/Scripts/minSide.js
+++ HardBank/HardBank/Scripts/minSide.js
@@ -6,4 +6,25 @@
         var s = "Sikker på at du vil overføre " + belop + ",- til kontoen: " + account + "?";
         $("p#modalMessage").text(s);
     });
+
+
+    $("#datepicker").datepicker({
+        firstDay: 1,
+        minDate: 0,
+        defaultDate:0
+    });
+
+    var switcher = 0;
+    $('#toggle-on').click(function () {
+        if (switcher == 0) {
+            $('#dateField').collapse('show');
+            switcher = 1;
+        } else if (switcher == 1) {
+            $('#dateField').collapse('hide');
+            switcher = 0;
+        }
+    });
+
+   
 });
+
