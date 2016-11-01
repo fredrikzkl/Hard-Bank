55e4f555cdf78638847ce728d5316053996be861
Magnus Bärnholt <magnus.barnholt@me.com>
2016-11-01 14:43:03 +0100

    AdminController

--- HardBank/HardAdmin/App_Start/RouteConfig.cs
+++ HardBank/HardAdmin/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HardAdmin
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Admin", action = "AdminSide", id = UrlParameter.Optional }
             );
         }
     }
---
+++ HardBank/HardAdmin/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HardAdmin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminSide()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
